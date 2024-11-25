4a023c31e49a7245e2dd6b17b4a3feee74f9aa91
Валерия <valeryastv@gmail.com>
2024-11-25 13:19:28 +0300

    Обновил дизайн главного экрана

---
+++ app/src/main/java/com/example/myapplication/introActivity.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.myapplication.databinding.ActivityIntroBinding;
+
+public class introActivity extends AppCompatActivity {
+ActivityIntroBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding=ActivityIntroBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.startBtn.setOnClickListener(v -> startActivity(new Intent(introActivity.this, MainActivity.class)));
+
+    }
+}
